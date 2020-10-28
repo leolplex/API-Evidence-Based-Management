46c48c1e542c8061911807c0b0419f455c508a5d
Daniel <raiga1234@gmail.com>
2020-10-27 21:20:47 -0500

    First commit

---
+++ src/main/java/com/ebm/EvidenceBasedManagementApplication.java
@@ -0,0 +1,13 @@
+package com.ebm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvidenceBasedManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvidenceBasedManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ebm/EvidenceBasedManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvidenceBasedManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
