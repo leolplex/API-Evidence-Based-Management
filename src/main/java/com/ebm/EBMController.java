e817a21e64a5b6cf03ccec8106ab77e6fcd29b15
Daniel <raiga1234@gmail.com>
2020-11-22 22:20:14 -0500

    return in change

--- src/main/java/com/ebm/EBMController.java
+++ src/main/java/com/ebm/EBMController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class EBMController {
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!!!s";
+        return "Greetings from Spring Boot!!! A change";
     }
 }
