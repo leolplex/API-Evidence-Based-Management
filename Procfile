a05a4e41711f8dc9a624b4b047a725d42684fc0c
Daniel <raiga1234@gmail.com>
2020-11-22 21:33:53 -0500

    Add deploy to Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar -Dspring.profiles.active=pdn build/libs/ebm-0.0.1.jar
---
+++ src/main/java/com/ebm/EBMController.java
@@ -0,0 +1,12 @@
+package com.ebm;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EBMController {
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+}
