6746c0807010f57c595d2b1c642224cdf255bbd3
Daniel <raiga1234@gmail.com>
2020-11-23 22:27:47 -0500

    Remove security hotspot

--- src/main/java/com/ebm/EBMController.java
+++ src/main/java/com/ebm/EBMController.java
@@ -1,11 +1,12 @@
 package com.ebm;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EBMController {
-    @RequestMapping("/")
+    @GetMapping("/hello")
     public String index() {
         return "Greetings from Spring Boot!!! A change";
     }
