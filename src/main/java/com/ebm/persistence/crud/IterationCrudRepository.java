9932b7e08e776a0e6e00cbbd87ae7e8a51fcc798
Daniel <raiga1234@gmail.com>
2020-12-07 17:06:20 -0500

    Remove code smell unused imports

--- src/main/java/com/ebm/persistence/crud/IterationCrudRepository.java
+++ src/main/java/com/ebm/persistence/crud/IterationCrudRepository.java
@@ -1,10 +1,7 @@
 package com.ebm.persistence.crud;
 
 import com.ebm.persistence.entity.EntityIteration;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-
-import java.util.List;
 
 public interface IterationCrudRepository extends CrudRepository<EntityIteration, Integer> {
 
--- src/main/java/com/ebm/persistence/crud/TeamCrudRepository.java
+++ src/main/java/com/ebm/persistence/crud/TeamCrudRepository.java
@@ -1,10 +1,8 @@
 package com.ebm.persistence.crud;
 
 import com.ebm.persistence.entity.EntityTeam;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
 
 public interface TeamCrudRepository extends CrudRepository<EntityTeam, Integer> {
 
