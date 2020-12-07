74b290165154b0ea7c6f2977aa60a3ca1ef6fa8e
Daniel <raiga1234@gmail.com>
2020-12-07 15:07:56 -0500

    Remove code smell public at unit test class

--- src/test/java/com/ebm/persistence/entity/EntityKVAAbilityToInnovateTest.java
+++ src/test/java/com/ebm/persistence/entity/EntityKVAAbilityToInnovateTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
-public class EntityKVAAbilityToInnovateTest {
+class EntityKVAAbilityToInnovateTest {
 
     EntityKVAAbilityToInnovate tester;
 
