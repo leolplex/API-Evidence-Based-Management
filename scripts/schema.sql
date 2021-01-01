15322ece449f681465c5ab8e3d3d31c3f7802725
Daniel <raiga1234@gmail.com>
2021-01-01 18:37:40 -0500

    Update property and fix database

--- scripts/schema.sql
+++ scripts/schema.sql
@@ -133,3 +133,28 @@ CREATE TABLE kva_unrealized_value
         ON DELETE NO ACTION
         NOT VALID
 )
+
+CREATE TABLE roles
+(
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    "Name" character varying COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT roles_pkey PRIMARY KEY (id)
+)
+
+
+
+CREATE TABLE users
+(
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    username character varying COLLATE pg_catalog."default" NOT NULL,
+    password character varying COLLATE pg_catalog."default" NOT NULL,
+    id_rol integer NOT NULL,
+    email character varying COLLATE pg_catalog."default",
+    CONSTRAINT users_pkey PRIMARY KEY (id),
+    CONSTRAINT users_username_key UNIQUE (username),
+    CONSTRAINT user_rol FOREIGN KEY (id_rol)
+        REFERENCES public.roles (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID
+)
