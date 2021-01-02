1332d7f2b06165851baca21e186198c0ad8d97d5
Daniel <raiga1234@gmail.com>
2021-01-02 11:45:46 -0500

    Fix security problem

--- src/main/java/com/ebm/web/controller/AppErrorController.java
+++ src/main/java/com/ebm/web/controller/AppErrorController.java
@@ -3,18 +3,21 @@ package com.ebm.web.controller;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Basic Controller which is called for unhandled errors
  */
 @Controller
+@CrossOrigin(origins = {"http://localhost:4200", "https://leolplex.github.io"}, methods = {RequestMethod.GET})
 public class AppErrorController implements ErrorController {
     private final static String PATH = "/error";
 
     @Override
-    @RequestMapping(PATH)
+    @RequestMapping(value = PATH, method = RequestMethod.GET)
     @ResponseBody
     public String getErrorPath() {
         return "No Mapping Found";
