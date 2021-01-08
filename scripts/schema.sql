fd90e09bab73109362d7f87c2942323ae3a7add4
Daniel <raiga1234@gmail.com>
2021-01-07 21:29:51 -0500

    Update schema.sql

--- scripts/schema.sql
+++ scripts/schema.sql
@@ -10,13 +10,20 @@ CREATE TABLE iteration
 )
 
 
-CREATE TABLE public.team
+CREATE TABLE team
 (
     id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
     name character varying COLLATE pg_catalog."default" NOT NULL,
     date_join timestamp with time zone,
-    CONSTRAINT team_pkey PRIMARY KEY (id)
+    id_user integer,
+    CONSTRAINT team_pkey PRIMARY KEY (id),
+    CONSTRAINT team_user FOREIGN KEY (id_user)
+        REFERENCES public.users (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID
 )
+
 
 
 CREATE TABLE iteration_team
