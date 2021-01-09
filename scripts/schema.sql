df3c5d570eec9bcbe67dc23b7904f73b48a172a9
Daniel <raiga1234@gmail.com>
2021-01-08 22:12:56 -0500

    Update script

--- scripts/schema.sql
+++ scripts/schema.sql
@@ -1,4 +1,5 @@
 
+
 CREATE TABLE iteration
 (
     id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
@@ -6,6 +7,7 @@ CREATE TABLE iteration
     start_date timestamp with time zone NOT NULL,
     end_date timestamp with time zone NOT NULL,
     state character varying COLLATE pg_catalog."default",
+    name character varying COLLATE pg_catalog."default" NOT NULL,
     CONSTRAINT iteration_pkey PRIMARY KEY (id)
 )
 
