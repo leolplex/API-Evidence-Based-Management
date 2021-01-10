e97913027a4bc7eed571b22cd1b1691fb83b20b3
Daniel <raiga1234@gmail.com>
2021-01-10 17:58:10 -0500

    Remove code smells

--- src/main/java/com/ebm/persistence/crud/ProductCrudRepository.java
+++ src/main/java/com/ebm/persistence/crud/ProductCrudRepository.java
@@ -3,9 +3,7 @@ package com.ebm.persistence.crud;
 import com.ebm.persistence.entity.EntityProduct;
 import org.springframework.data.repository.CrudRepository;
 
-import javax.swing.text.html.parser.Entity;
 import java.util.List;
-import java.util.Optional;
 
 public interface ProductCrudRepository extends CrudRepository<EntityProduct, Integer> {
 
