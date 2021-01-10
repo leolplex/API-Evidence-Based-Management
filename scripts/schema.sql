460b00d199f75a39ad0e0c39af17a757a7f90104
Daniel <raiga1234@gmail.com>
2021-01-10 18:36:41 -0500

    Update script product

--- scripts/schema.sql
+++ scripts/schema.sql
@@ -27,20 +27,26 @@ CREATE TABLE team
 )
 
 
+
 CREATE TABLE product
 (
     id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
     name character varying COLLATE pg_catalog."default" NOT NULL,
     start_date timestamp with time zone NOT NULL,
     id_team integer NOT NULL,
+    id_user integer NOT NULL,
     CONSTRAINT product_pkey PRIMARY KEY (id),
+    CONSTRAINT fk_user FOREIGN KEY (id_user)
+        REFERENCES public.users (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID,
     CONSTRAINT product_team FOREIGN KEY (id_team)
         REFERENCES public.team (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
         NOT VALID
 )
-
 
 
 CREATE TABLE iteration_product
